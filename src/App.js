4aa7b234ddddd1dc95871bb3288e9332b8cc3b3f
Levansarajishvili <120027976+Levansarajishvili@users.noreply.github.com>
2024-06-03 17:15:03 +0400

    movienew

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Movie from './Movie';
+
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    getMovies(API_URL);
+  }, []);
+
+  const getMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setMovies(data.results);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm && searchTerm !== '') {
+      getMovies(SEARCH_API + searchTerm);
+      setSearchTerm('');
+    }
+  };
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <form onSubmit={handleSubmit} id="form">
+          <input
+            type="text"
+            className="search"
+            id="search"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={handleChange}
+          />
+        </form>
       </header>
+      <main id="main">
+        {movies.length > 0 && movies.map((movie) => (
+          <Movie key={movie.id} {...movie} />
+        ))}
+      </main>
     </div>
   );
 }
